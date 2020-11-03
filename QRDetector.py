7cfa96ed1d5ae832ab57683521579778c8a2bec2
Naimul Hoque <55862319+naimulhq@users.noreply.github.com>
2020-11-03 11:55:02 -0800

    NH - Developed code which detects QR Codes
    Laptop Camera is used to detect QR Codes and put bounding box around each QR Codes with text associated with code.

---
+++ QRDetector.py
@@ -0,0 +1,34 @@
+# QR Detector which uses OpenCV to capture live feed and pyzbar to detect QR Codes
+import cv2
+from pyzbar.pyzbar import decode
+
+# Create a video capture object. Necessary to capture video from webcam. 0 = front camera. 1 for back camera
+cap = cv2.VideoCapture(1)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Get all QR Codes in frame
+    QRCodes = decode(frame)
+    
+    # If length is not 0, put a bounding box on each code
+    if(len(QRCodes) != 0):
+        # Iterate through each Decoded Object in QRCodes List
+        for i in range(len(QRCodes)):
+            x,y,w,h = QRCodes[i].rect.left, QRCodes[i].rect.top, QRCodes[i].rect.width, QRCodes[i].rect.height # Get coordinates
+            string = (QRCodes[i].data).decode('ASCII') # Get Text Data
+            frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(36,255,12), 1) # Create a rectangle on the frame
+            cv2.putText(frame, string, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2) # Add text with decoded QR Code text.
+        cv2.imshow('frame',frame) # Output image
+    else:
+        cv2.imshow('frame',frame) # Output image.
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
