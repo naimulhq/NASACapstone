5eae8f6c7c2b82c47835f869c93ac3eecd6a0ed5
naimulhq <naimul@ucsb.edu>
2020-11-07 21:43:29 -0800

    NH - Added Raspberry Pi Cam Compatibility

--- QRDetector.py
+++ QRDetector.py
@@ -2,8 +2,37 @@
 import cv2
 from pyzbar.pyzbar import decode
 
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+
 # Create a video capture object. Necessary to capture video from webcam. 0 = front camera. 1 for back camera
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 
 Green = (36,255,12)
 Red = (0,0,255)
