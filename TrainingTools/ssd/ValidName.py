100f17b89e5d04127212ea2ca72f8e555d643da3
Naimul Hoque <55862319+naimulhq@users.noreply.github.com>
2021-01-04 11:52:44 -0800

    NH - Validates xml files to ensure proper labeling.

---
+++ TrainingTools/ssd/ValidName.py
@@ -0,0 +1,28 @@
+from xml.dom import minidom
+import os
+import time
+
+## Enter directory where all xml files are in.
+
+directory = "C:\\Users\\18186\\Downloads\\xmls\\xmls";
+
+for _,_,files in os.walk(directory):
+    pass
+
+incorrectFiles = []
+labels = ['Motor','Top Plate', 'Board', 'Bottom Plate', 'F2 Bracket', 'F3 Bracket', 'M3X15 Standoff', 'M3X30 Standoff', 'Mounting Plate', 'Stage 1.2', 'Stage 2.1'
+    'Stage 2.2', 'Stage 3.2', 'Stage 4.1', 'Stage 4.3', 'Stage 5.1', 'Stage 5.2', 'Stage 5.6']
+
+for f in files:
+    my_xml = minidom.parse(directory+'\\'+f)
+    objects = my_xml.getElementsByTagName('object')
+    for obj in objects:
+        name = obj.getElementsByTagName('name')
+        if name[0].firstChild.data not in labels:
+            custom_str = "\nIncorrect File: " + str(f)  + "--------- Supposed Label: " + str(name[0].firstChild.data)
+            incorrectFiles.append(custom_str)
+            
+f = open('IncorrectFiles.txt','w')
+f.writelines(incorrectFiles)
+f.close()
+
