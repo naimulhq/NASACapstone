e1d8a49be1b3b83884671aa8c8b36476b53af2df
naimulhq <naimul@ucsb.edu>
2021-01-24 01:34:36 -0800

    NH - Retrieved Data on Bounding Boxes

--- src/my-detection.py
+++ src/my-detection.py
@@ -23,19 +23,48 @@
 
 import jetson.inference
 import jetson.utils
+import time
+from datetime import datetime
+import os
 # Might need to change paths for --model and --labels.
 # If no boxes appear, lower threshold.
 
-# This net used with custom model 
-#net = jetson.inference.detectNet(argv=['--model=./ssd-mobilenet.onnx','--labels=./labels.txt','--input_blob=input_0','--output-cvg=scores','--output-bbox=boxes','--threshold=.5'])
+# Get parent directory. Necessary to load model correctly
 
-# This net used with models provided by NVIDIA
-#net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+
+# Open up labels file for part detection
+f = open("labels.txt","r")
+labels = []
+for i in f.readlines():
+	labels.append(i.strip('\n'))
+f.close()
+
+# This net used with Part Detection.
+# Change model directory depending on user. Stores labels in same directory as src
+net = jetson.inference.detectNet(argv=['--model=/home/naimulhq/Capstone/models/PartDetection/ssd-mobilenet-2.03.onnx','--labels=./labels.txt','--input_blob=input_0','--output-cvg=scores','--output-bbox=boxes','--threshold=.8'])
 camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
+# Add another net, camera, and display for Stage Detection
+beginTime = time.time()
+endTime = 0
+
 while display.IsStreaming():
+	# Keep Track of Time
+	beginTime = time.time()
 	img = camera.Capture()
-	detections = net.Detect(img)
+	detections = net.Detect(img) # Holds all the valuable Information
+	
+	# If difference greater than log time desired in seconds, log the data. Currently, logging data every five seconds
+	if(beginTime-endTime > 1):
+		objects = []
+		for i in range(len(detections)):
+			objects.append(labels[detections[i].ClassID])
+		print(objects)
+		now = datetime.now()
+		current_time = now.strftime("%H:%M:%S")
+		print(current_time)
+		endTime = time.time()
+		
 	display.Render(img)
 	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
