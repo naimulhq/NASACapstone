d3512f115fc8a0c059a51e4bdd78c52a61005eca
olesbober <ragekage23424@gmail.com>
2021-02-17 21:25:33 -0800

    OB - updated ValidName.py

--- TrainingTools/ssd/ValidName.py
+++ TrainingTools/ssd/ValidName.py
@@ -4,18 +4,17 @@ import time
 
 ## Enter directory where all xml files are in.
 
-directory = "/home/oles/Documents/Capstone/TrainingTools/ssd/Annotations/";
+directory = "/home/oles/Capstone/TrainingTools/ssd/Annotations/";
 
 for _,_,files in os.walk(directory):
     pass
 
 incorrectFiles = []
-labels = ['Motor','Top Plate', 'Board', 'Bottom Plate', 'F2 Bracket', 'F3 Bracket', 'M3X15 Standoff', 'M3X30 Standoff', 'Mounting Plate', 'Stage 1.2', 'Stage 2.1'
-    'Stage 2.2', 'Stage 3.2', 'Stage 4.1', 'Stage 4.3', 'Stage 5.1', 'Stage 5.2', 'Stage 5.6']
+labels = ['Motor','Top Plate', 'Board', 'Bottom Plate', 'F2 Bracket', 'F3 Bracket', 'M3X15 Standoff', 'M3X30 Standoff', 'Mounting Plate', 'Stage 1.2', 'Stage 2.1', 'Stage 2.2', 'Stage 3.2', 'Stage 4.1', 'Stage 4.3', 'Stage 5.1', 'Stage 5.2', 'Stage 5.6 (No FTDI)']
 
 for f in files:
-    my_xml = minidom.parse(directory+f)
     print(f)
+    my_xml = minidom.parse(directory+f)
     objects = my_xml.getElementsByTagName('object')
     for obj in objects:
         name = obj.getElementsByTagName('name')
