0abb023e73d50727a78a7ab989f8d7f1d8dd908a
edwinvarela <edwinvarela@ucsb.edu>
2021-03-28 15:18:36 -0700

    EV, OB, and NH - Added camera feed and button text display to the GUI

--- Gui/gui.py
+++ Gui/gui.py
@@ -4,9 +4,13 @@ from kivy.config import Config
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', 1214)
 Config.set('graphics', 'height', 743)
+from kivy.logger import Logger
+import logging
+Logger.setLevel(logging.TRACE)
 
 import kivy
 from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.label import Label
 #from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -15,24 +19,85 @@ from kivy.uix.button import Button
 #from kivy.uix.screenmanager import ScreenManager, Screen
 #from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.core.camera import Camera
+from kivy.graphics.texture import Texture
+from kivy.uix.image import Image
 
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.textfield import MDTextFieldRect
+import cv2
 #Using KV
+
+########################################################Use this for pi Cam
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=20,
+    flip_method=2,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+############################################################################3
 class MyGrid(Widget):
-	def pressed_validate(self):
-		print("Validate")
+	def print_button_text(self,label):
+		label.text += "poop"
+		#instance.text
 
-	def pressed_forward(self):
-		print("Forward")
+	def pressed_validate(self,label):
+		label.text += "Validate\n"
 
-	def pressed_previous(self):
-		print("Previous")
+
+	def pressed_forward(self,label):
+		label.text += "Forward\n"
+
+	def pressed_previous(self,label):
+		label.text += "Previous\n"
+
 	
 
-class Project_Argus(App):
+class Project_Argus(MDApp):
 	def build(self):
 		return MyGrid()
 
-
+class KivyCamera(Image):
+    def __init__(self, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        # self.capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+        self.capture = cv2.VideoCapture(0)# EDWHINE YES DATS ME
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        self.clock = Clock.schedule_interval(self.update, 1.0 / 20)
+        print("KivyCam set")
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            # convert it to texture
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            # display image from the texture
+            self.texture = image_texture
 
 if __name__ == "__main__":
 	Project_Argus().run()
