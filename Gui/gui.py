48667ec697786513318e85cff27874cc5b6f24ad
naimulhq <naimul@ucsb.edu>
2021-04-17 14:02:17 -0700

    NH - Seperate Parts and Stages Model

--- Gui/gui.py
+++ Gui/gui.py
@@ -7,6 +7,7 @@ Config.set('graphics', 'height', 743)	# DON'T CHANGE THIS
 # from kivy.logger import Logger
 # import logging
 # Logger.setLevel(logging.TRACE)
+
 import kivy
 from kivy.app import App
 from kivy.graphics import Color
@@ -35,6 +36,7 @@ import time
 from datetime import datetime
 from functools import partial
 
+
 class ProcedureScreen(Screen):
     def pressed_forward(self,cam,label):
         if (cam.currentInstr < len(cam.instructions)-1):
@@ -46,6 +48,7 @@ class ProcedureScreen(Screen):
         else:
             label.text += "Cannot go forward. The procedure is complete.\n\nClose window or return to main menu.\n\n"
 
+
     def pressed_previous(self,cam,label):
         if (cam.currentInstr > 0):
             cam.currentInstr -= 1
@@ -53,8 +56,10 @@ class ProcedureScreen(Screen):
         else:
             label.text += "Currently on first instruction. Cannot go back a stage.\n\n"
 
+
     def beginValidation(self,cam,label):
         if (cam.currentInstr < len(cam.instructions)):
+            cam.isValidate = True
             label.text += "Begin Validation\n\n"
             cam.clock2 = Clock.schedule_interval(partial(cam.stageValidate, label), 1.0/20)
         else:
@@ -106,7 +111,9 @@ class MainMenu(Screen):
         global button_exist
 
         if(button_exist==False):
-            button = Button(text="Begin",size_hint_y=None,height=100,size_hint_x=None,width=400, pos=(810,3))
+            button = Button(text="Begin",size_hint_y=None,
+                            height=100,size_hint_x=None,
+                            width=400, pos=(810,3))
             button.bind(on_press=self.go_to_procedure)
             grid.add_widget(button)
             button_exist=True
@@ -132,12 +139,14 @@ class MainMenu(Screen):
                     count += 1
                 label.text += "     - " + str(row[1]) + ": " + str(row[0]) + '\n'
         
+
 class Project_Argus(MDApp):
     def build(self):
         self.sm = ScreenManager()
         self.sm.add_widget(MainMenu(name='mainMenu'))
         self.sm.add_widget(ProcedureScreen(name='procedureScreen'))
         return self.sm
+
 
 class KivyCamera(Image):
     def __init__(self, **kwargs):
@@ -153,20 +162,21 @@ class KivyCamera(Image):
         part_model_path = '/home/'+ str(dirs[0]) + '/Capstone/models/PartDetection/All_Parts.onnx'
         stage_model_path = '/home/'+ str(dirs[0]) + '/Capstone/models/Stages/All_Stages.onnx'
 
-	    #os.system("sudo modprobe v4l2loopback") for Rishit
-	    #os.system("ffmpeg -thread_queue_size 512 -i rtsp://192.168.1.1/MJPG -vcodec rawvideo -vf scale=1920:1080 -f v4l2 -threads 0 -pix_fmt yuyv422 /dev/video1") for Rishit
-	    #time.sleep(5)
+	#os.system("sudo modprobe v4l2loopback") for Rishit
+	#os.system("ffmpeg -thread_queue_size 512 -i rtsp://192.168.1.1/MJPG -vcodec rawvideo -vf scale=1920:1080 -f v4l2 -threads 0 -pix_fmt yuyv422 /dev/video1") for Rishit
+	#time.sleep(5)
         # This net used with Part Detection.
         # Change model directory depending on user. Stores labels in same directory as src
 
         self.part_net = jetson.inference.detectNet(argv=['--model='+part_model_path,'--labels=./labels_parts.txt','--input_blob=input_0','--output-cvg=scores','--output-bbox=boxes','--threshold=.9'])
         self.stages_net = jetson.inference.detectNet(argv=['--model='+stage_model_path,'--labels=./labels_stages.txt','--input_blob=input_0','--output-cvg=scores','--output-bbox=boxes','--threshold=.9'])
-        #self.camera = jetson.utils.videoSource("csi://0") #csi://0 
-        self.camera = jetson.utils.videoSource("/dev/video0")# '/dev/video0'for Edwin '/dev/video1' for Rishit
+        self.camera = jetson.utils.videoSource("csi://0") #csi://0 
+        #self.camera = jetson.utils.videoSource("/dev/video0")# '/dev/video0'for Edwin '/dev/video1' for Rishit
         #self.display = jetson.utils.videoOutput() # 'my_video.mp4' for file
         self.clock = Clock.schedule_interval(self.update, 1.0 / 20)
         self.clock2 = None
         self.isComplete = False
+        self.isValidate = False
 
         with open(os.path.join(sys.path[0], "instructions.csv"),'r') as file:
             reader = csv.reader(file)
@@ -185,8 +195,10 @@ class KivyCamera(Image):
 
     def update(self, dt):
         self.img = self.camera.Capture()
-        self.detections = self.part_net.Detect(self.img) # Holds all the valuable Information
-        self.stages = self.stages_net.Detect(self.img)
+        if not self.isValidate:
+            self.detections = self.part_net.Detect(self.img) # Holds all the valuable Information
+        else:
+            self.stages = self.stages_net.Detect(self.img)
         array = jetson.utils.cudaToNumpy(self.img)
         buf1 = cv2.flip(array,0)
         buf = buf1.tostring()
@@ -205,16 +217,11 @@ class KivyCamera(Image):
                 self.stageCount = 0
 
             if self.stageCount == 48:
-                self.stageCount = 0
+                self.isValidate = False
                 self.currentInstr += 1
-
-                #label.text += "self.currentInstr: " + str(self.currentInstr) + " " + str(len(self.instructions))
-                if self.currentInstr < len(self.instructions):
-                    #self.currentInstr = len(self.instructions) - 1
+                self.stageCount = 0
+                if(self.currentInstr < len(self.instructions)):
                     label.text += "Validation Successful\n\n\n" + self.instructions[self.currentInstr][1] + ": " + self.instructions[self.currentInstr][0] + "\n\n"
-                elif self.currentInstr >= len(self.instructions):
-                    label.text += "Procedure Complete!\n\nClose window or return to main menu.\n\n"
-
                 Clock.unschedule(self.clock2)
                 sc = app.sm.get_screen("procedureScreen")
                 if self.currentInstr <= 0:
@@ -254,11 +261,16 @@ class KivyCamera(Image):
                     sc.ids.checklist.text += self.instructions[self.currentInstr+1][1] + "\n\n"
                     sc.ids.checklist.text += self.instructions[self.currentInstr+2][1] + "\n\n"
 
-        if self.timeout == 900:
+        if self.timeout == 400:
+            self.isValidate = False
             label.text += "Validation Unsuccessful. Time expired!\n\n"
             Clock.unschedule(self.clock2)
             self.timeout = 0
+            
+			
 
 if __name__ == "__main__":
     app=Project_Argus()
     app.run()
+
+
